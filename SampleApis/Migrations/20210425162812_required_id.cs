9b65537f32c6ad152c2964d86642aa0c8f9476f4
Joever <joever.monceda@gmail.com>
2021-04-25 23:37:37 +0700

    add_migration for required id

---
+++ SampleApis/Migrations/20210425162812_required_id.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SampleApis.Migrations
+{
+    public partial class required_id : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
